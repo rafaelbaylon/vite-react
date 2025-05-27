109a31130c5c2a75259c4c5c46ad406ebb551ec7
Rafael Baylon II <rafael.g.baylonII@gmail.com>
2025-05-27 18:31:47 +0800

    edit index.html title

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
